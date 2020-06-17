52d0cabc8f3d35c6447db646a84baffe028e284f
Augusto Rodrigo <augustor.camblor@gmail.com>
2020-06-17 18:06:11 -0300

    Ultima Atualização
    não esquecer de refazer o projeto pelos bugs

---
+++ Open World CScripts and Bullets/Open World CScripts and Bullets/Assets/Scripts/SomDiretoAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomDiretoAnim : MonoBehaviour
+{
+
+    ControladorSom controleSom;
+
+    private void Awake()
+    {
+        ControladorSom = GameObject.FindWithTag("MainCamera").GetComponent<ControladorSom>();
+    }
+
+    public void AnimEvent_Ouvir_Walk()
+    {
+        controleSom.Ouvir(ControladorSom.Ouvir.Andar);
+    }
+    public void AnimEvent_Ouvir_Hit()
+    {
+        controleSom.Ouvir(ControladorSom.Ouvir.Atack);
+    }
+}
---
+++ Open World CScripts and Bullets/Open World CScripts and Bullets/Assets/Scripts/Tripe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tripe : MonoBehaviour
+{
+    Transform transfPlayer;
+    Transform transf;
+
+    public float speedRotation;
+
+    void Awake()
+    {
+        transf = GetComponent<Transform>();
+
+        GameObject playerGameObject = GameObject.FindWithTag("Player");
+        transfPlayer = playerGameObject.GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        transf.position = transfPlayer.position;
+
+        float mvtX = Input.GetAxis("Mouse X");
+        transf.Rotate(0, mvtX * speedRotation * Time.deltaTime, 0);
+    }
+}
